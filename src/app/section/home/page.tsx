61fbb5d8d87cf4f780fb5137a6eb1f3984bc195f
T-Poison10 <dipansilwal10@gmail.com>
2024-02-22 23:55:38 +0545

    responsive prototype

--- src/app/section/home/page.tsx
+++ src/app/section/home/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import MainLogo from "@/app/assets/icons/MainLogo";
 import HeroIcon from "@/app/assets/icons/Hero";
+import HeroPhone from "@/app/assets/icons/Hero"
 import { cabinetBold } from "@/app/utils/fonts";
 import { useState, useEffect } from 'react';
 
@@ -35,7 +36,8 @@ export default function Hero() {
   return (
     <div className=" flex flex-column justify-center object-contain bg-dark-purple ">
         <div className=" w-screen h-screen overflow-hidden object-fill border border-black">
-            <HeroIcon className=""/>
+            <HeroIcon className="sm:hidden md:hidden lg:block"/>
+            <HeroPhone className="lg:hidden md:block sm:block" />
         </div>
       
       <div className="absolute gap-[25px] top-[80px] flex flex-col text-center ">
