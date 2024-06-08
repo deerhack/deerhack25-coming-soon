a035250096ee5a8fdbc5a22d00d89d8e6c558f84
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-06-08 16:03:27 +0545

    fix: basepath for deployment to pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath:"/deerhack24-coming-soon",
     output:'export',
     images:{
         unoptimized:true
     },
-    // assetPrefix:'/deerhack24-coming-soon'
+    // assetPrefix:'/deerhack24-coming-soon',
 };
 
 export default nextConfig;
