3326000c1c7a7763e56dcd86de0b67d6c6760600
pra-naya <85820204+pra-naya@users.noreply.github.com>
2025-03-08 09:36:28 +0545

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath:"/deerhack25-coming-soon",
+    basePath:"",
     output:'export',
     images:{
         unoptimized:true
