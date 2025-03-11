cd8171cdfe73af45f7e3e01837bceb4f8d939e30
Srijan Dahal <srijan102dahal@gmail.com>
2025-03-12 01:20:52 +0545

    fix: coming soon page

--- src/app/components/ComingSoonCard.tsx
+++ src/app/components/ComingSoonCard.tsx
@@ -6,11 +6,11 @@ import { ReactElement } from "react";
 export default function ComingSoonCard(): ReactElement {
   return (
     <div
-      className={`${styles.muted_color} rounded-[0.65rem] w-[80vw] lg:w-[35vw] xl:w-[30vw] h-[30vh] lg:h-[45vh] flex flex-col gap-7 justify-center items-center  border-secondary border-2 backdrop-blur-sm px-2 py-3`}
+      className={`${styles.muted_color} rounded-[0.65rem] w-[80vw] lg:w-[35vw] xl:w-[30vw] h-[32vh] lg:h-[45vh] flex flex-col gap-7 justify-center items-center  border-secondary border-2 backdrop-blur-sm px-2 py-3`}
     >
-      <MainLogo height={100} width={400} />
+      <MainLogo height={135} width={400} className="" />
       <div
-        className={` ${cabinetExtraBold.className} text-center text-4xl text-primary-gradient-color`}
+        className={` ${cabinetExtraBold.className} text-center lg:text-5xl text-4xl text-primary-gradient-color`}
       >
         Coming Soon
       </div>
