c3adf245f750f17f8416c3d90bf826f851747485
Srijan Dahal <srijan102dahal@gmail.com>
2025-03-12 00:51:02 +0545

    fix: coming soon page

--- src/app/utils/config.ts
+++
@@ -1,6 +0,0 @@
-export const supaBaseConfig = {
-  projectUrl: "https://fifnzxukjdzyymlygked.supabase.co/",
-  supabaseKey:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpZm56eHVramR6eXltbHlna2VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzNjE1MzMsImV4cCI6MjA1NjkzNzUzM30.fNT-4olQKZaCy__Rn4EiyCFdLsy6Emj7MMoMqnodYqY",
-};
-
